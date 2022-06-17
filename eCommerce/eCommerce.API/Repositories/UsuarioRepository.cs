0ee4cc0c38423300b6fd05e1cbb8cdf6ff760aa5
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-06-17 08:17:34 -0300

    Criando a API

---
+++ eCommerce/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,49 @@
+﻿using eCommerce.API.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario(){ Id=1,Nome="Bruno Ferreira",Email="bruno@email.com"},
+            new Usuario(){ Id=2,Nome="Vinicius Dantas",Email="vini@email.com"},
+            new Usuario(){ Id=3,Nome="Vivian Servelo",Email="vivian@email.com"}
+        };
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+
+        public Usuario Get(int id)
+        {
+            return _db.FirstOrDefault(u => u.Id == id);
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            var ultimoUsuario = _db.LastOrDefault();
+            if (ultimoUsuario == null)
+            {
+                usuario.Id = 1;
+            }
+            else
+            {
+                usuario.Id = ultimoUsuario.Id;
+                usuario.Id++;
+            }
+            _db.Add(usuario);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(_db.FirstOrDefault(u => u.Id == usuario.Id));
+            _db.Add(usuario);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(_db.FirstOrDefault(u => u.Id == id));
+        }
+    }
+}
